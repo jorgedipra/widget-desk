1221e301e335ac8655275fbec043ad5670a1e83b
Jorge Edwin Diaz <jorgedipra@gmail.com>
2024-08-20 19:49:35 -0500

    ➕ REFACTOR: 🌐 CSS Unificación y mejora en el proceso de Gulp para componentes CSS
    ✔Descripción: Se realizaron mejoras en la estructura de CSS mediante unificación de estilos, así como una refactorización del proceso de compilación utilizando Gulp. Esto permitió mejorar la consistencia visual y facilitar la gestión de los componentes CSS.

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,18 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('js')); // Carpeta de destino
 });
 
+// Definir la tarea de combinar archivos CSS
+gulp.task('styles', function() {
+    return gulp.src('stylos/components/*.css') // Ruta a tus archivos CSS
+        .pipe(concat('components.css')) // Nombre del archivo combinado
+        .pipe(gulp.dest('stylos')); // Carpeta de destino
+});
 
 // Definir la tarea de vigilancia
 gulp.task('watch', function() {
     gulp.watch('js/components/*.js', gulp.series('scripts')); // Observa cambios en archivos JavaScript
+    gulp.watch('stylos/components/*.css', gulp.series('styles')); // Observa cambios en archivos CSS
 });
 
 // Definir la tarea por defecto
-gulp.task('default', gulp.series('scripts', 'watch'));
+gulp.task('default', gulp.series('scripts', 'styles', 'watch'));
